62eeb6c0e249aa198dd32e110bf95cea4229514d
Marcelino <38365875+pedroMarcelino@users.noreply.github.com>
2023-03-28 12:37:41 -0300

    Aula de componentes
    Abri um state com name e counte, fiz a substituicao ao clica no <p> e um contador ao clicar no link

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
@@ -7,11 +8,35 @@ class App extends Component {
     counter: 0
   };
 
+
+  handlePClick = () => {
+    this.setState({ name: 'Teste' })
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  }
+
   render() {
     const { name, counter } = this.state
     return (
       < div className="App" >
-
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick}>
+            Olá, {name}, {counter}
+          </p>
+          <a onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
