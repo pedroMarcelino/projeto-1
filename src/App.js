0411e12075cd80f847c3dcbb910160bc72f697b4
Marcelino <38365875+pedroMarcelino@users.noreply.github.com>
2023-03-29 12:45:03 -0300

    Props e organização dos componentes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import { PostCard } from './components/PostCard';
 
 class App extends Component {
   state = {
@@ -37,13 +38,7 @@ class App extends Component {
       <section className='container'>
         <div className="posts" >
           {posts.map(post => (
-            <div className="post">
-              <img src={post.cover} alt={post.title} />
-              <div key={post.id} className="post-content">
-                <h1>{post.title}</h1>
-                <p>{post.body}</p>
-              </div>
-            </div>
+            <PostCard key={post.id} post={post} />
           ))}
         </div>
       </section>
